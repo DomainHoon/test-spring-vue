60a139e9ae8e4a36a0f169820778d2411fbf3f92
RootSong <sgh8774@gmail.com>
2022-03-27 21:42:06 +0900

    controller and vue test

--- src/main/java/com/example/testspringvue/controller/TestController.java
+++ src/main/java/com/example/testspringvue/controller/TestController.java
@@ -1,11 +1,12 @@
 package com.example.testspringvue.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-
+    @GetMapping("/api/test")
     public String test() {
         return "1234";
     }
